1f61b7e8a3c5da72c3c52fd071f4cdbccbc99895
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-05-13 17:52:02 -0300

    Update

--- Sala01.cpp
+++ Sala01.cpp
@@ -2,27 +2,31 @@
 using namespace std;
 int main() {
     int pessoas, media;
-    int sala[6][6];
+    int sala[6][2];
     
-    cout << "Informe quantos alunos estudam na sala 1: " << endl;
-    cin >> sala[0][0];
-    cout << "Informe quantos alunos estudam na sala 2: " << endl;
-    cin >> sala[1][1];
-    cout << "Informe quantos alunos estudam na sala 3: " << endl;
-    cin >> sala[2][2];
-    cout << "Informe quantos alunos estudam na sala 4: " << endl;
-    cin >> sala[3][3];
-    cout << "Informe quantos alunos estudam na sala 5: " << endl;
-    cin >> sala[4][4];
-    cout << "Informe quantos alunos estudam na sala 6: " << endl;
-    cin >> sala[5][5];
+    sala[0][0] = 1;
+    sala[0][1] = 35;
+    sala[1][0] = 2;
+    sala[1][1] = 4;
+    sala[2][0] = 3;
+    sala[2][1] = 22;
+    sala[3][0] = 4;
+    sala[3][1] = 20;
+    sala[4][0] = 5;
+    sala[4][1] = 36;
+    sala[5][0] = 6;
+    sala[5][1] = 30;
     
+      cout << "Informe quantos alunos estudam na sala 1: " << sala[0][1] << endl;
+    cout << "Informe quantos alunos estudam na sala 2: " << sala[1][1] << endl;
+    cout << "Informe quantos alunos estudam na sala 3: " << sala[2][1] <<endl;
+    cout << "Informe quantos alunos estudam na sala 4: " << sala[3][1] << endl;
+    cout << "Informe quantos alunos estudam na sala 5: " << sala[4][1]<< endl;
+    cout << "Informe quantos alunos estudam na sala 6: " << sala[5][1]<< endl;
     
-    media = (sala[0][0] + sala[1][1] + sala[2][2] + sala[3][3] + sala[4][4] + sala[5][5])/6; 
-    cout << "Resultado: " << media<< endl;
-
-    
-    cout << "As salas com alunos acima da média possuem: " << sala[0][0] << ", "<< sala[4][4] << ", e "<< sala[5][5] << " alunos."<< endl;
-    
+     media = ((sala[0][1] + sala[1][1] + sala[2][1] + sala[3][1] + sala[4][1] + sala[5][1])/6);
+     cout << "A média de alunos é: " << media << endl;
+     
+  
   return 0;
 }
