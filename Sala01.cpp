3c8068d35aad5cb73e9b901bd2e7d69899d163f4
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-05-15 13:36:32 -0300

    Update Sala01.cpp

--- Sala01.cpp
+++ Sala01.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int pessoas, media;
-    int sala[6][2];
     
-    sala[0][0] = 1;
-    sala[0][1] = 35;
-    sala[1][0] = 2;
-    sala[1][1] = 4;
-    sala[2][0] = 3;
-    sala[2][1] = 22;
-    sala[3][0] = 4;
-    sala[3][1] = 20;
-    sala[4][0] = 5;
-    sala[4][1] = 36;
-    sala[5][0] = 6;
-    sala[5][1] = 30;
+    int media, pessoas;
+    int sala[6];
     
-      cout << "Informe quantos alunos estudam na sala 1: " << sala[0][1] << endl;
-    cout << "Informe quantos alunos estudam na sala 2: " << sala[1][1] << endl;
-    cout << "Informe quantos alunos estudam na sala 3: " << sala[2][1] <<endl;
-    cout << "Informe quantos alunos estudam na sala 4: " << sala[3][1] << endl;
-    cout << "Informe quantos alunos estudam na sala 5: " << sala[4][1]<< endl;
-    cout << "Informe quantos alunos estudam na sala 6: " << sala[5][1]<< endl;
+    sala[0] = 35;
+    sala[1] = 4;
+    sala[2] = 22;
+    sala[3] = 20;
+    sala[4] = 36;
+    sala[5] = 30;
     
-     media = ((sala[0][1] + sala[1][1] + sala[2][1] + sala[3][1] + sala[4][1] + sala[5][1])/6);
-     cout << "A média de alunos é: " << media << endl;
-     
-  
-  return 0;
+    for (int i = 0; i < 6; i++){
+        cout << "Sala " << i + 1 << ": " << sala[i] << endl;
+    }
+    
+    media = (sala[0] + sala[1] + sala[2] + sala[3] + sala[4] + sala[5]);
+    media = (media/6);
+    
+    cout << "A media de alunos por sala é: " << (media) << endl;
+    
+return 0;
 }
