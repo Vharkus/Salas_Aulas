cb869404c6289279ea94232c6e676ff40903b7cc
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-05-15 15:06:13 -0300

    Sala02.cpp

--- Sala02.cpp
+++ Sala02.cpp
@@ -1,11 +1,12 @@
+//Biblioetas
 #include <iostream>
 using namespace std;
 
 int main() {
-    
-    int media, pessoas;
+    //Declaração das variáveis junto do array.
+    int media;
     int sala[6];
-    
+    //Interfaces interativas que permitem o usuário definir o número de alunos.
     cout << "Informe quantos alunos tem na sala 1: "<< endl;
     cin >> sala[0];
     cout << "Informe quantos alunos tem na sala 2: " << endl;
@@ -18,15 +19,32 @@ int main() {
     cin >> sala[4];
     cout << "Informe quantos alunos tem na sala 6: " << endl;
     cin >> sala[5];
-
+    //Bloco de repetição que lista as salas e números de alunos os colocando em ordem.
     for (int i = 0; i < 6; i++){
+        //Interface repetitiva que mostrará as salas.
         cout << "Sala " << i + 1 << ": " << sala[i] << endl;
         
     }
+    //Equação que nos da a média de alunos.
      media = (sala[0] + sala[1] + sala[2] + sala[3] + sala[4] + sala[5]);
     media = (media/6);
     
-    cout << "A media de alunos por sala é: " << media << endl;
-    
+    cout << "Salas com alunos acima da média: " << endl;
+    //Segundo bloco de repetição For.
+    for (int i = 0; i < 6; i++){
+        //Caso o número de alunos seja maior que o número da média, a repetição será acionada.
+        if (sala[i] > media){
+            //Interface que mostra as salas com os maiores números.
+            cout << "Sala " << i + 1 << " ,";
+    }
+}
+    cout << "Salas com alunos abaixo da média: " << endl;
+    //Terceiro bloco de repetição For.
+    for (int i = 0; i < 6; i++){
+        //Mesmo esquema do for anterior, mas dessa vez vai mostrar as salas com números menores que a média. 
+        if (sala[i] < media){
+            cout << "Sala " << i + 1 << " ,";
+        }
+    }
 return 0;
 }
